521df74eb8ba6c1e68bda2bafec6c82e41f63822
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-08-20 20:10:45 +0200

    consolidate results

--- R/01_hymenoptera_analysis.R
+++ R/01_hymenoptera_analysis.R
@@ -1,18 +1,10 @@
-data_hym %>%
-  dplyr::group_by(DRUH, SITMAP) %>%
-  dplyr::reframe(pocet_n = n(),
-                 pocet_inat = sum(str_detect(ZDROJ, "iNat")),
-                 proc_inat = pocet_inat/pocet_n) %>%
-  dplyr::group_by(DRUH) %>%
-  dplyr::reframe(
-    celkem_sitmap = n(),
-    sitmap_plne_inat = sum(proc_inat == 1),
-    procento_plne_inat = sitmap_plne_inat / celkem_sitmap
+# Hymenoptera - all species ----
+inat_hym_sitmap0 <- 
+  data_hym %>%
+  dplyr::group_by(
+    DRUH, 
+    SITMAP
   ) %>%
-  dplyr::arrange(-procento_plne_inat) 
-
-data_hym_rl %>%
-  dplyr::group_by(DRUH, SITMAP) %>%
   dplyr::reframe(
     pocet_n = n(),
     pocet_inat = sum(str_detect(ZDROJ, "iNat")),
@@ -20,9 +12,137 @@ data_hym_rl %>%
     ) %>%
   dplyr::group_by(DRUH) %>%
   dplyr::reframe(
-    celkem_sitmap = n(),
-    sitmap_plne_inat = sum(proc_inat == 1),
-    procento_plne_inat = sitmap_plne_inat / celkem_sitmap
+    celkem_sitmap_0 = n(),
+    sitmap_0_plne_inat = sum(proc_inat == 1),
+    procento_0_plne_inat = sitmap_0_plne_inat / celkem_sitmap_0
   ) %>%
-dplyr::arrange(-procento_plne_inat) 
+  dplyr::arrange(
+    desc(procento_0_plne_inat)
+  ) 
+
+inat_hym_sitmap1 <- 
+  data_hym %>%
+  dplyr::group_by(
+    DRUH, 
+    SITMAP_1
+    ) %>%
+  dplyr::reframe(
+    pocet_n = n(),
+    pocet_inat = sum(str_detect(ZDROJ, "iNat")),
+    proc_inat = pocet_inat/pocet_n
+    ) %>%
+  dplyr::group_by(DRUH) %>%
+  dplyr::reframe(
+    celkem_1_sitmap = n(),
+    sitmap_1_plne_inat = sum(proc_inat == 1),
+    procento_1_plne_inat = sitmap_1_plne_inat / celkem_1_sitmap
+  ) %>%
+  dplyr::arrange(
+    desc(procento_1_plne_inat)
+    ) 
+
+inat_hym_result <- 
+  dplyr::left_join(
+    inat_hym_sitmap0,
+    inat_hym_sitmap1
+  ) %>%
+  dplyr::arrange(
+    desc(procento_1_plne_inat),
+    desc(sitmap_1_plne_inat),
+    desc(celkem_1_sitmap)
+  ) %>%
+  dplyr::left_join(
+    data_hym %>%
+      dplyr::select(
+        DRUH,
+        REDLIST
+      ) %>%
+      sf::st_drop_geometry()
+  ) %>%
+  sf::st_drop_geometry() %>%
+  dplyr::distinct()
+
+# Hymenoptera - Red list species ----
+inat_hym_rl_sitmap0 <-
+  data_hym_rl %>%
+  dplyr::group_by(
+    DRUH, 
+    SITMAP
+  ) %>%
+  dplyr::reframe(
+    pocet_n = n(),
+    pocet_inat = sum(str_detect(ZDROJ, "iNat")),
+    proc_inat = pocet_inat/pocet_n
+  ) %>%
+  dplyr::group_by(DRUH) %>%
+  dplyr::reframe(
+    celkem_sitmap_0 = n(),
+    sitmap_0_plne_inat = sum(proc_inat == 1),
+    procento_0_plne_inat = sitmap_0_plne_inat / celkem_sitmap_0
+  ) %>%
+  dplyr::arrange(
+    desc(procento_0_plne_inat)
+  ) 
+
+inat_hym_rl_sitmap1 <-
+  data_hym_rl %>%
+  dplyr::group_by(
+    DRUH, 
+    SITMAP_1
+    ) %>%
+  dplyr::reframe(
+    pocet_n = n(),
+    pocet_inat = sum(str_detect(ZDROJ, "iNat")),
+    proc_inat = pocet_inat/pocet_n
+    ) %>%
+  dplyr::group_by(DRUH) %>%
+  dplyr::reframe(
+    celkem_1_sitmap = n(),
+    sitmap_1_plne_inat = sum(proc_inat == 1),
+    procento_1_plne_inat = sitmap_1_plne_inat / celkem_1_sitmap
+  ) %>%
+dplyr::arrange(
+  desc(procento_1_plne_inat)
+  ) 
+
+inat_hym_rl_result <- 
+  dplyr::left_join(
+  inat_hym_rl_sitmap0,
+  inat_hym_rl_sitmap1
+) %>%
+  dplyr::arrange(
+    desc(procento_1_plne_inat)
+  ) %>%
+  dplyr::left_join(
+    data_hym_rl %>%
+      dplyr::select(
+        DRUH,
+        REDLIST
+      ) %>%
+      sf::st_drop_geometry()
+  ) %>%
+  sf::st_drop_geometry() %>%
+  dplyr::distinct()
+
+# Write results ----
+write_csv2_win1250 <- function(x, path, ...) {
+  # write UTF-8 to a temporary file
+  tmp <- tempfile(fileext = ".csv")
+  readr::write_csv2(x, tmp, ...)
+  
+  # re-encode to Windows-1250
+  txt <- readr::read_file(tmp)
+  txt <- iconv(txt, from = "UTF-8", to = "Windows-1250")
+  readr::write_file(txt, path)
+}
+
+write_csv2_win1250(
+  inat_hym_result,
+  "Outputs/Data/inat_hym_result.csv"
+)
+
+write_csv2_win1250(
+  inat_hym_rl_result,
+  "Outputs/Data/inat_hym_rl_result.csv"
+)
 
