d1157e83f5a61d7892c284cbd0a5f1637074f5e6
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-08-24 10:45:06 +0200

    simplify package reading

--- R/00_config_inat.R
+++ R/00_config_inat.R
@@ -1,88 +1,18 @@
-# LOAD PACKAGES ----
-if(!isTRUE(require(tidyverse, quietly = TRUE))) {
-  install.packages("tidyverse", dependencies = TRUE); library(tidyverse)
-} else {
-  require(tidyverse)}
+# LOAD DATA ----
+load_or_install <- function(pkg) {
+  if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+    library(pkg, character.only = TRUE)
+  }
+}
 
-if(!isTRUE(require(sf, quietly = TRUE))) {
-  install.packages("sf", dependencies = TRUE); library(sf)
-} else {
-  require(sf)}
+pkgs <- c(
+  "tidyverse", "sf", "sp", "proj4", "matrixStats", "leaflet", "lubridate",
+  "condformat", "openxlsx", "raster", "stars", "ggsn", "zoo", "rgdal", 
+  "grid", "gridGraphics", "officer", "flextable"
+)
 
-if(!isTRUE(require(sp, quietly = TRUE))) {
-  install.packages("sp", dependencies = TRUE); library(sp)
-} else {
-  require(sp)}
-
-if(!isTRUE(require(proj4, quietly = TRUE))) {
-  install.packages("proj4", dependencies = TRUE); library(proj4)
-} else {
-  require(proj4)}
-
-if(!isTRUE(require(matrixStats, quietly = TRUE))) {
-  install.packages("matrixStats", dependencies = TRUE); library(matrixStats)
-} else {
-  require(matrixStats)}
-
-if(!isTRUE(require(leaflet, quietly = TRUE))) {
-  install.packages("leaflet", dependencies = TRUE); library(leaflet)
-} else {
-  require(leaflet)}
-
-if(!isTRUE(require(lubridate, quietly = TRUE))) {
-  install.packages("lubridate", dependencies = TRUE); library(lubridate)
-} else {
-  require(lubridate)}
-
-if(!isTRUE(require(condformat, quietly = TRUE))) {
-  install.packages("condformat", dependencies = TRUE); library(condformat)
-} else {
-  require(condformat)}
-
-if(!isTRUE(require(openxlsx, quietly = TRUE))) {
-  install.packages("openxlsx", dependencies = TRUE); library(openxlsx)
-} else {
-  require(openxlsx)}
-
-if(!isTRUE(require(raster, quietly = TRUE))) {
-  install.packages("raster", dependencies = TRUE); library(raster)
-} else {
-  require(raster)}
-
-if(!isTRUE(require(stars, quietly = TRUE))) {
-  install.packages("stars", dependencies = TRUE); library(stars)
-} else {
-  require(stars)}
-
-if(!isTRUE(require(ggsn, quietly = TRUE))) {
-  install.packages("ggsn", dependencies = TRUE); library(ggsn)
-} else {
-  require(ggsn)}
-
-if(!isTRUE(require(zoo, quietly = TRUE))) {
-  install.packages("zoo", dependencies = TRUE); library(zoo)
-} else {
-  require(zoo)}
-
-if(!isTRUE(require(rgdal, quietly = TRUE))) {
-  install.packages("rgdal", dependencies = TRUE); library(rgdal)
-} else {
-  require(rgdal)}
-
-if(!isTRUE(require(ggsn, quietly = TRUE))) {
-  install.packages("ggsn", dependencies = TRUE); library(ggsn)
-} else {
-  require(ggsn)}
-
-if(!isTRUE(require(grid, quietly = TRUE))) {
-  install.packages("grid", dependencies = TRUE); library(grid)
-} else {
-  require(grid)}
-
-if(!isTRUE(require(gridGraphics, quietly = TRUE))) {
-  install.packages("gridGraphics", dependencies = TRUE); library(gridGraphics)
-} else {
-  require(gridGraphics)}
+invisible(lapply(pkgs, load_or_install))
 
 write_csv2_win1250 <- function(x, path, ...) {
   # write UTF-8 to a temporary file
@@ -95,7 +25,6 @@ write_csv2_win1250 <- function(x, path, ...) {
   readr::write_file(txt, path)
 }
 
-# LOAD DATA ----
 #--------------------------------------------------#
 ## Load remote data -----
 #--------------------------------------------------#
