f743f2a0c4643576c38f2e58233719878e7bf382
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-09-07 22:24:07 +0200

    init plots

--- R/02_Plots.R
+++ R/02_Plots.R
@@ -2,8 +2,48 @@
 ggplot(
   data = inat_result, 
   aes(
-    x = KAT_TAX, 
+    x = REDLIST, 
     y = procento_1_plne_inat,
-    colour = REDLIST)
+    colour = KAT_TAX)
 ) +
   geom_boxplot()
+
+
+inat_result %>%
+  dplyr::select(
+    DRUH, 
+    REDLIST,
+    procento_0_plne_inat
+    ) %>%
+  dplyr::filter(
+    procento_0_plne_inat > 0
+  ) %>%
+  pivot_longer(
+    cols = starts_with("procento"),
+    names_to = "level",
+    values_to = "percent"
+  ) %>%
+  mutate(
+    level = ifelse(level == "procento_0_plne_inat", "SITMAP")
+    ) %>%
+  ggplot(aes(x = reorder(DRUH, percent), y = percent, fill = REDLIST)) +
+  geom_col(position = "dodge") +
+  coord_flip() +
+  labs(
+    x = "Species",
+    y = "% of SITMAP fully iNat",
+    fill = "Level"
+  ) +
+  theme_minimal()
+
+ggplot(inat_result, aes(x = procento_0_plne_inat, y = procento_1_plne_inat)) +
+  geom_point(aes(size = celkem_1_sitmap, color = KAT_TAX), alpha = 0.7) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+  labs(
+    x = "% fully iNat (SITMAP)",
+    y = "% fully iNat (SITMAP_1)",
+    color = "Taxon",
+    size = "N SITMAP_1"
+  ) +
+  theme_minimal()
+
